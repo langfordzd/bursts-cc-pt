dcd29fa424b7d8ab56e5718e8676bf1f786fb7fc
langfordzd <langford.z.d@gmail.com>
2022-06-10 16:37:47 +0200

    Add files via upload

--- plot3.py
+++ plot3.py
@@ -540,3 +540,391 @@ def plot_freq_check(lfp,lfp_bs,lfp_trials,lfp_ids,ecog,ecog_bs,ecog_trials,ecog_
         xtr.set_ylabel('S/R bursts')#, fontsize=12)
         xtr.yaxis.set_label_coords(0.925, 0.5)       
         ####################
+    #%%    
+def plot_chars_behav(ecog,marco,pablo,v):
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+    import seaborn as sns
+    
+    
+    
+    fig = plt.figure(1, figsize=(12, 12))
+    gs = gridspec.GridSpec(12, 12)
+    gs.update(wspace=0.8, hspace=0.9)
+    ################################################################################################
+    ############################################################################### 
+
+    ####################
+    ecog_u = ecog[~(ecog['which'] == 'co')]
+    lfp_u = pablo[~(pablo['which'] == 'co')]
+    marco_u = marco[~(marco['which'] == 'co')]
+    lfp_u = lfp_u.astype({'which':object,'amp':float,'freq':float,'duration':float,'behav':object})
+    ecog_u = ecog_u.astype({'which':object,'amp':float,'freq':float,'duration':float,'behav':object})
+    marco_u = marco_u.astype({'which':object,'amp':float,'freq':float,'duration':float,'behav':object})
+
+    xtr = fig.add_subplot(gs[0:2, 6:8])
+    xtr.text(0, 0.7, "C", fontsize=20, fontweight="bold", va="bottom", ha="left",
+          transform=xtr.transAxes)
+    xtr = sns.violinplot(x="which", y="duration", hue="behav",
+                        data=lfp_u, palette=v, split=True,inner="quartile")
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,0.5])
+    xtr.set_yticks([0, 0.5])
+    xtr.set_yticklabels(['0','0.5'])
+    xtr.yaxis.set_label_position("right")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+    xtr.text(0.8, 0.9, "*", fontsize=16, va="bottom", ha="left",
+      transform=xtr.transAxes)    
+
+
+    xtr = fig.add_subplot(gs[2:4, 6:8])
+    xtr = sns.violinplot(x="which", y="amp", hue="behav",
+                        data=lfp_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,1])
+    xtr.set_yticks([0, 1])
+    xtr.set_yticklabels(['0','1.0'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+
+
+    xtr = fig.add_subplot(gs[4:6, 6:8])
+    xtr = sns.violinplot(x="which", y="freq", hue="behav",
+                        data=lfp_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([10,30])
+    xtr.set_yticks([10, 30])
+    xtr.set_yticklabels(['10','30'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_ylabel('')#, fontsize=12)
+    xtr.set_xlabel('LFP(P)')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+
+    xtr = fig.add_subplot(gs[0:2, 8:10])
+    xtr = sns.violinplot(x="which", y="duration", hue="behav",
+                        data=marco_u, palette=v, split=True,inner="quartile")
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,0.5])
+    xtr.set_yticks([0, 0.5])
+    xtr.set_yticklabels(['0','0.5'])
+    xtr.yaxis.set_label_position("right")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+
+
+    xtr = fig.add_subplot(gs[2:4, 8:10])
+    xtr = sns.violinplot(x="which", y="amp", hue="behav",
+                        data=marco_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,1])
+    xtr.set_yticks([0, 1])
+    xtr.set_yticklabels(['0','1.0'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+
+    xtr = fig.add_subplot(gs[4:6, 8:10])
+    xtr = sns.violinplot(x="which", y="freq", hue="behav",
+                        data=marco_u, palette=v, split=True, inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([10,30])
+    xtr.set_yticks([10, 30])
+    xtr.set_yticklabels(['10','30'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_ylabel('')#, fontsize=12)
+    xtr.set_xlabel('LFP(M)')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+    xtr.text(0.1, 0.9, "*", fontsize=16, va="bottom", ha="left",
+          transform=xtr.transAxes)
+    xtr.text(0.8, 0.9, "*", fontsize=16, va="bottom", ha="left",
+      transform=xtr.transAxes)
+
+    
+    xtr = fig.add_subplot(gs[0:2, 10:12])
+    xtr = sns.violinplot(x="which", y="duration", hue="behav",
+                        data=ecog_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,0.5])
+    xtr.set_yticks([0, 0.5])
+    xtr.set_yticklabels(['0','0.5'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.set_ylabel('Duration')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+    xtr.text(0.1, 0.9, "*", fontsize=16, va="bottom", ha="left",
+      transform=xtr.transAxes)    
+
+    
+    xtr = fig.add_subplot(gs[2:4, 10:12])
+    xtr = sns.violinplot(x="which", y="amp", hue="behav",
+                        data=ecog_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([0,1])
+    xtr.set_yticks([0, 1])
+    xtr.set_yticklabels(['0','1.0'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('')#, fontsize=12)
+    xtr.set_ylabel('Amplitude')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+    xtr.text(0.8, 0.9, "*", fontsize=16, va="bottom", ha="left",
+      transform=xtr.transAxes)        
+    
+
+
+    xtr = fig.add_subplot(gs[4:6, 10:12])
+    xtr = sns.violinplot(x="which", y="freq", hue="behav",
+                        data=ecog_u, palette=v, split=True,inner="quartile")
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.set_ylim([10,30])
+    xtr.set_yticks([10, 30])
+    xtr.set_yticklabels(['10','30'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('ECoG')#, fontsize=12)
+    xtr.set_ylabel('Frequency')#, fontsize=12)
+    xtr.get_legend().remove()#%%
+    xtr.set_xticklabels(['CC','PTA'])
+    handles, labels = xtr.get_legend_handles_labels()
+    xtr.legend(handles=handles[0:],frameon=False, labels=['S','R'],bbox_to_anchor=(1.5, -0.1))
+    xtr.text(0.8, 0.9, "*", fontsize=16,  va="bottom", ha="left",
+      transform=xtr.transAxes)    
+    plt.show()
+#%%    
+def plot_behav(lfp,lfp_bs,lfp_trials,lfp_ids,ecog,ecog_bs,ecog_trials,ecog_ids,restrict):
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+    import seaborn as sns
+    import pandas as pd 
+    
+    pt_lfp_b, pt_lfp_c      = behav(lfp,lfp_bs,lfp_trials,'pta', lfp_ids)
+    cc_lfp_b, cc_lfp_c      = behav(lfp,lfp_bs,lfp_trials,'cc', lfp_ids)
+    pt_ecog_b, pt_ecog_c    = behav(ecog,ecog_bs,ecog_trials,'pta',ecog_ids)
+    cc_ecog_b, cc_ecog_c    = behav(ecog,ecog_bs,ecog_trials,'cc',ecog_ids)
+    
+    qs = ['#191970','#DC143C','#FF8247']
+    count_lfp = pd.concat([pt_lfp_c,cc_lfp_c])
+    count_ecog = pd.concat([pt_ecog_c,cc_ecog_c])
+    behav_lfp = pd.concat([pt_lfp_b,cc_lfp_b])
+    behav_lfp['monkey'] = behav_lfp['loc'].astype(str).str[0]
+    behav_ecog = pd.concat([pt_ecog_b,cc_ecog_b])
+    
+    
+    fig = plt.figure(1, figsize=(13, 13))
+    gs = gridspec.GridSpec(13, 13)
+    gs.update(wspace=0.8, hspace=0.9)
+    ################################################################################################
+    ############################################################################### 
+    xtr = fig.add_subplot(gs[1:2, 0:2])
+    xtr.text(0, 1.325, "A", fontsize=20, fontweight="bold", va="bottom", ha="left",
+                  transform=xtr.transAxes)
+    clfp = count_lfp[count_lfp['behav']=='search']
+    xtr = sns.countplot(data = clfp, x='count',hue='which', palette=[qs[1],qs[0]])
+    xtr.set_ylim([0, 2500])
+    xtr.set_xlim([-0.5, 4.5])
+    xtr.spines['right'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_yticks([0, 2500])
+    xtr.set_yticklabels(['0','1500'])
+    xtr.set_xticks([0, 1, 2, 3, 4])
+    xtr.set_xticklabels(['0', '1', '2','3','4'])
+    xtr.tick_params(axis="both",direction="in")
+    xtr.set(ylabel='S',xlabel='')
+    xtr.yaxis.set_label_coords(-.05, 0.5)
+    handles, labels = xtr.get_legend_handles_labels()
+    xtr.legend(handles=handles[0:], labels=labels[0:])
+    handles, labels = xtr.get_legend_handles_labels()
+    xtr.legend(handles=handles[0:],frameon=False, labels=['PTA','CC'],bbox_to_anchor=(1.0, 0.95))
+    
+    xtr = fig.add_subplot(gs[2:3, 0:2])
+    clfp = count_lfp[count_lfp['behav']=='repeat']
+    xtr = sns.countplot(data = clfp, x='count',hue='which', palette=[qs[1],qs[0]])
+    xtr.set_ylim([0, 1500])
+    xtr.set_xlim([-0.5, 4.5])
+    xtr.spines['right'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_yticks([0, 1500])
+    xtr.set_yticklabels(['0','1500'])
+    xtr.set_xticks([0, 1, 2, 3, 4])
+    xtr.set_xticklabels(['0', '1', '2','3','4'])
+    xtr.tick_params(axis="both",direction="in")
+    xtr.set(ylabel='R',xlabel='# bursts')
+    xtr.yaxis.set_label_coords(-.05, 0.5)
+    xtr.get_legend().remove()
+
+    xtr = fig.add_subplot(gs[0:4, 2:7])
+    palette = {"cc": qs[0], "pta": qs[1]}
+    kws = {"s": 60, "facecolor": "none", "linewidth": 1.5}
+    xtr = sns.scatterplot(
+        data=behav_lfp, x="ratio_behav", y="ratio_burst", 
+        edgecolor=behav_lfp["which"].map(palette),#style='monkey',
+        **kws,
+    )
+    handles, labels = zip(*[
+        (plt.scatter([], [], ec=color, **kws), key) for key, color in palette.items()
+    ])
+    xtr.legend(handles=handles[0:], labels=['CC','PTA'], bbox_to_anchor=(0.25, 0.75),frameon=False)
+    
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.plot([0, 2], [0, 2], 'k-', lw=1)
+    xtr.set_xlim([0,2])
+    xtr.set_ylim([0,2])
+    xtr.set_yticks([0, 0.5, 1, 1.5, 2])
+    xtr.set_yticklabels(['0','','','','2'])
+    xtr.set_xticks([0, 0.5, 1, 1.5, 2])
+    xtr.set_xticklabels(['0','','','','2'])
+    xtr.set(ylabel='',xlabel='')
+    xtr.yaxis.set_label_position("right")
+    xtr.set_ylabel('S/R bursts')#, fontsize=12)
+    xtr.set_xlabel('S/R Trials')#, fontsize=12)
+    xtr.xaxis.set_label_coords(0.5, 0.1)   
+    xtr.yaxis.set_label_coords(0.925, 0.5)   
+    #############################################################################################################################################
+    xtr = fig.add_subplot(gs[5:6, 0:2])
+    xtr.text(0, 1.325, "B", fontsize=20, fontweight="bold", va="bottom", ha="left",
+              transform=xtr.transAxes)
+    cecog = count_ecog[count_ecog['behav']=='search']
+    xtr = sns.countplot(data = cecog, x='count',hue='which', palette=[qs[1],qs[0]])
+    xtr.set_ylim([0, 1500])
+    xtr.set_xlim([-0.5, 4.5])
+    xtr.spines['right'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_yticks([0, 1500])
+    xtr.set_yticklabels(['0','1500'])
+    xtr.set_xticks([0, 1, 2, 3, 4])
+    xtr.set_xticklabels(['0', '1', '2','3','4'])
+    xtr.tick_params(axis="both",direction="in")
+    xtr.set(ylabel='S',xlabel='')
+    xtr.xaxis.set_label_coords(0.5, -0.15)
+    xtr.yaxis.set_label_coords(-0.05, 0.5)
+    xtr.get_legend().remove()
+    
+    xtr = fig.add_subplot(gs[6:7, 0:2])
+    cecog = count_ecog[count_ecog['behav']=='repeat']
+    xtr = sns.countplot(data = cecog, x='count',hue='which', palette=[qs[1],qs[0]])
+    xtr.set_ylim([0, 7000])
+    xtr.set_xlim([-0.5, 4.5])
+    xtr.spines['right'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.set_yticks([0, 1000, 2000, 3000, 4000, 5000, 6000,7000])
+    xtr.set_yticklabels(['0','','','','','','','7000'])
+    xtr.set_xticks([0, 1, 2, 3, 4])
+    xtr.set_xticklabels(['0', '1', '2','3','4'])
+    xtr.tick_params(axis="both",direction="in")
+    xtr.set(ylabel='R',xlabel='# bursts')
+    xtr.yaxis.set_label_coords(-0.05, 0.5)
+    xtr.get_legend().remove()
+    
+    xtr = fig.add_subplot(gs[4:8, 2:7])
+    xtr = sns.scatterplot(
+        data=behav_ecog, x="ratio_behav", y="ratio_burst", 
+        edgecolor=behav_ecog["which"].map(palette),
+        **kws,
+    )
+    xtr.spines['left'].set_visible(False)
+    xtr.spines['top'].set_visible(False)
+    xtr.yaxis.tick_right()
+    xtr.plot([0, 1], [0, 1], 'k-', lw=1)
+    xtr.set_xlim([0,1])
+    xtr.set_ylim([0,1])
+    xtr.set_yticks([0, 0.5, 1, 1])
+    xtr.set_yticklabels(['0','','','1'])
+    xtr.set_xticks([0, 0.5, 1, 1])
+    xtr.set_xticklabels(['0','','','1'])
+    xtr.yaxis.set_label_position("right")
+    xtr.set_xlabel('S/R Trials')#, fontsize=12)
+    xtr.xaxis.set_label_coords(0.5, 0.1)   
+    xtr.set_ylabel('S/R bursts')#, fontsize=12)
+    xtr.yaxis.set_label_coords(0.925, 0.5)       
+    ####################
+    positions = [[0,2,7,10,4,6,7,10],
+                 [0,2,10,13,4,6,10,13],
+                 [2,4,7,10,6,8,7,10],
+                 [2,4,10,13,6,8,10,13]]
+    
+    for pos, r in zip(positions,restrict):
+        lfpf = lfp[(lfp['freq']>r[0]) & (lfp['freq']<r[1]) ]
+        ecogf = ecog[(ecog['freq']>r[0]) & (ecog['freq']<r[1])]
+        pt_lfp_b, pt_lfp_c      = behav(lfpf,lfp_bs,lfp_trials,'pta', lfp_ids)
+        cc_lfp_b, cc_lfp_c      = behav(lfpf,lfp_bs,lfp_trials,'cc', lfp_ids)
+        pt_ecog_b, pt_ecog_c    = behav(ecogf,ecog_bs,ecog_trials,'pta',ecog_ids)
+        cc_ecog_b, cc_ecog_c    = behav(ecogf,ecog_bs,ecog_trials,'cc',ecog_ids)
+        lfpo = pd.concat([pt_lfp_b,cc_lfp_b])
+        lfpo['monkey'] = lfpo['loc'].astype(str).str[0]
+        ecogo = pd.concat([pt_ecog_b,cc_ecog_b])
+        behav_lfp = lfpo#[(lfpo['freq']>r[0]) & (lfpo['freq']<r[1]) ]
+        behav_ecog = ecogo#[(ecogo['freq']>r[0]) & (ecogo['freq']<r[1])]
+
+    
+    
+        xtr = fig.add_subplot(gs[pos[0]:pos[1], pos[2]:pos[3]])
+        palette = {"cc": qs[0], "pta": qs[1]}
+        kws = {"s": 60, "facecolor": "none", "linewidth": 1.5}
+        xtr = sns.scatterplot(
+            data=behav_lfp, x="ratio_behav", y="ratio_burst", 
+            edgecolor=behav_lfp["which"].map(palette),#style='monkey',
+            **kws,
+        )
+        title = str(r[0])+'-'+str(r[1])+' Hz'
+        xtr.set_title(title, x=0.2,y=0.8, pad=-14, rotation = 33)
+
+        xtr.spines['left'].set_visible(False)
+        xtr.spines['top'].set_visible(False)
+        xtr.yaxis.tick_right()
+        xtr.plot([0, 2], [0, 2], 'k-', lw=1)
+        xtr.set_xlim([0,2])
+        xtr.set_ylim([0,2])
+        xtr.set_yticks([0, 0.5, 1, 1.5, 2])
+        xtr.set_yticklabels(['0','','','','2'])
+        xtr.set_xticks([0, 0.5, 1, 1.5, 2])
+        xtr.set_xticklabels(['0','','','','2'])
+        xtr.set(ylabel='',xlabel='')
+
+        xtr = fig.add_subplot(gs[pos[4]:pos[5], pos[6]:pos[7]])        
+        xtr = sns.scatterplot(
+            data=behav_ecog, x="ratio_behav", y="ratio_burst", 
+            edgecolor=behav_ecog["which"].map(palette),
+            **kws,
+        )
+        title = str(r[0])+'-'+str(r[1])+' Hz'
+        xtr.set_title(title, x=0.2,y=0.8, pad=-14, rotation = 33)
+        xtr.spines['left'].set_visible(False)
+        xtr.spines['top'].set_visible(False)
+        xtr.yaxis.tick_right()
+        xtr.plot([0, 1], [0, 1], 'k-', lw=1)
+        xtr.set_xlim([0,1])
+        xtr.set_ylim([0,1])
+        xtr.set_yticks([0, 0.5, 1, 1])
+        xtr.set_yticklabels(['0','','','1'])
+        xtr.set_xticks([0, 0.5, 1, 1])
+        xtr.set_xticklabels(['0','','','1'])
+        xtr.set(ylabel='',xlabel='')
+
